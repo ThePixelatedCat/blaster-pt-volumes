9b5c496e2076922ec9df91890ccb697ceb87b675
ThePixelatedCat <acfrostie@gmail.com>
2025-07-14 20:05:14 +0200

    Updating table design

---
+++ main.js
@@ -0,0 +1,37 @@
+let data = [
+    new Blaster( 'Knight-L', '130', '36', "Printkeep!" ),
+    new Blaster( 'Lynx 2.x', '100.0125', '34.925', "Orion Blasters" ),
+    new Blaster( 'Falx', '120', '34.925', "FLP" )
+]
+
+function Blaster ( name, draw, id, maker ) {
+    this.name = name;
+    this.draw = draw;
+    this.id = id;
+    this.maker = maker;
+ 
+    this.volume = function () {
+        return ((Math.PI * ((this.id / 2) ** 2) * this.draw) / 1000).toFixed(2);
+    }
+};
+
+$('#data-table').DataTable( {
+    data,
+    columns: [
+        { data: 'name', className: 'dt-center' },
+        { data: 'volume', className: 'dt-center', searchable: false },
+        { data: 'draw', className: 'dt-center', searchable: false },
+        { data: 'id', className: 'dt-center', searchable: false },
+        { data: 'maker', className: 'dt-center' }
+    ],
+    scrollY: '650px',
+    scrollCollapse: true,
+    paging: false,
+    language: {
+        search: 'Search by Name or Maker:'
+    },
+    layout: {
+        topStart: 'info',
+        bottomStart: null
+    }
+} );
